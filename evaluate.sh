3389602734566e63e3fada932f460ee94435f184
ingyuseong <dkc984@likelion.org>
2022-04-06 12:30:47 +0000

    Add ERASER benchmark code

--- evaluate.sh
+++ evaluate.sh
@@ -7,5 +7,5 @@ echo "----------------Generate test_decoded.jsonl----------------"
 python3 test_decoded_generator.py
 
 echo "----------------Evaluate based on ERASER benchmark(TF1, IOUF1)----------------"
-PYTHONPATH=$PY_PATH python rationale_benchmark/metrics.py --split test --data_dir data --results output/test_decoded.jsonl --score_file output/test_scores.json
+PYTHONPATH=$PY_PATH python eraserbenchmark/rationale_benchmark/metrics.py --split test --data_dir data --results output/test_decoded.jsonl --score_file output/test_scores.json
 mv eraserbenchmark/output/test_scores.json output/eraser_scores.json
